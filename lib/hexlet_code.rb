f7ac4e0b691640fa9f22fe4c1bce4d49d43096af
Dmitrii Arsenov <dmitry.arsyonov@mail.ru>
2025-08-24 22:03:54 +0200

    fix

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,8 +6,8 @@ module HexletCode
   class Error < StandardError; end
   autoload :Tag, File.expand_path("hexlet_code/tag.rb", __dir__)
 
-  def self.form_for(user, *attrs)
+  def self.form_for(user, attrs = {})
     url = attrs.delete(:url) || "#"
-    Tag.build("form", { action: url, method: "post" }.merge(attrs))
+    Tag.build("form", action: url, method: "post", **attrs)
   end
 end
