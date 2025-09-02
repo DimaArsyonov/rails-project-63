cfb9bc9c321a055eab15e2fd88cf3ce5304e1be7
Dmitrii Arsenov <dmitry.arsyonov@mail.ru>
2025-09-02 23:01:29 +0200

    fix of block

--- lib/hexlet_code/tags/input.rb
+++ lib/hexlet_code/tags/input.rb
@@ -4,7 +4,7 @@ module HexletCode
   module Tags
     class Input
       autoload :Tag, File.expand_path("../tag.rb", __dir__)
-      def self.build(name, value, attrs, &block)
+      def self.build(name, value, attrs, &_block)
         final_attrs = attrs.except(attrs.key(value), "type")
         HexletCode::Tag.build("input", { name: name, type: "text", value: value }.merge(final_attrs))
       end
