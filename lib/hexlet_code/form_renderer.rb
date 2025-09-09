bb28f3855a0cbfa907b01d3f1ddc291c533f488f
Dmitrii Arsenov <dmitry.arsyonov@mail.ru>
2025-09-09 23:37:46 +0200

    hope final fixes for second review

--- lib/hexlet_code/form_renderer.rb
+++ lib/hexlet_code/form_renderer.rb
@@ -5,21 +5,25 @@ require_relative 'tag'
 module HexletCode
   # FormRenderer — class responsible for rendering final data from FormBuilder
   class FormRenderer
+    def self.input_for_html(input)
+      label_html = Tag.build('label', for: input[:name]) { input[:name].to_s.capitalize }
+
+      tag_class = "HexletCode::Tags::#{input[:as].to_s.capitalize}".constantize
+      input_html = tag_class.build(input[:name], input[:value], input[:attrs])
+
+      "#{label_html}#{input_html}"
+    end
+
     def self.render_html(form_builder)
       body_html = []
       form_body = form_builder.form_body
       form_body[:inputs].each do |input|
-        label_html = Tag.build('label', for: input[:name]) { input[:name].to_s.capitalize }
-
-        tag_class = "HexletCode::Tags::#{input[:as].to_s.capitalize}".constantize
-        input_html = tag_class.build(input[:name], input[:value], input[:attrs])
-
-        body_html << "#{label_html}#{input_html}"
+        body_html << input_for_html(input)
       end
 
       body_html << Tag.build('input', type: 'submit', **form_body[:submit][:options]) if form_body[:submit][:options]
 
-      Tag.build('form', form_builder.form_body[:form_options]) { body_html.join('') }
+      Tag.build('form', form_builder.form_body[:form_options]) { body_html.join }
     end
   end
 end
